1115556460f882e3da30b255f443584c9d28a013
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-24 19:54:47 +0500

    Select and SelectMany

---
+++ linq1/linq1/Emp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq1
+{
+    public class Emp
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public decimal Salary { get; set; }
+
+        public List<string> programming { get; set; }
+        public static List<Emp> Employees()
+        {
+            List<Emp> emps = new List<Emp>()
+            {
+                new Emp(){ID = 1, firstName="Jeck", lastName = "Maa", Salary = 60000, programming=new List<string>(){"java","c","c++" } },
+                new Emp(){ID = 2, firstName="Ale", lastName = "Sas", Salary = 50000, programming=new List<string>(){ "c#", "c", "c++" } },
+                new Emp() { ID = 3, firstName = "MArk", lastName = "Suck", Salary = 80000, programming = new(){ "html", "css", "c#" } },
+                new Emp(){ID = 4, firstName="Ilon", lastName = "MAsk", Salary = 40000, programming=new(){"kotlin","python","linq" } },
+                new Emp(){ID = 5, firstName="Ali", lastName = "Qushchi", Salary = 70000, programming=new(){"c#","c","c++" } }
+
+            };
+
+            return emps;
+        }
+
+
+
+
+    }
+}
--- linq1/linq1/Program.cs
+++ linq1/linq1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace linq1
+﻿using System.Data;
+
+namespace linq1
 {
     internal class Program
     {
@@ -9,13 +11,158 @@
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
             };
 
-            var linkResult = from item in numbers
-                             where item % 2 == 0
-                             select item;
+            #region 1
+            //var linkResult = from item in numbers
+            //                 where item % 2 == 0
+            //                 select item;
+            #endregion
 
-            foreach (var item in linkResult)
+            #region 2
+
+            //var result1 = from linq in numbers
+            //              where linq % 2 == 0
+            //              where linq % 3 == 0
+            //              select linq;
+
+            #endregion
+
+            #region 3
+
+            #endregion
+            //var result2 = numbers.Where(x => x % 2 == 0).Count();
+            //Console.WriteLine(result2);
+            //Console.WriteLine();
+
+            //query syntax
+            //1
+            //Select1();
+
+            //2
+            // Select2();
+
+            //3
+            //SelectMany1();
+
+            //4
+            //SelectMany2();
+
+            //5
+            SelectMany3();
+
+
+
+
+        }
+        static void Select1()
+        {
+            var resultData = (from item in Emp.Employees()
+                              select item.ID).ToList();
+
+            foreach (var item in resultData)
+            {
+                Console.WriteLine($"id:{item}");
+            }
+
+            var data = Emp.Employees().Select(emp => emp.ID).ToList();
+
+            foreach (var item in data)
+            {
+                Console.WriteLine("id:" + item);
+            }
+        }
+        static void Select2()
+        {
+            //query
+            var result = (from emp in Emp.Employees()
+                          select new Emp()
+                          {
+                              firstName = emp.firstName,
+                              lastName = emp.lastName,
+                              Salary = emp.Salary
+                          });
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"FirstName:{item.firstName}, LastName:{item.lastName}, Salary:{item.Salary}");
+            }
+            // method
+            var result2 = Emp.Employees().Select(emp => new Emp()
+            {
+                firstName = emp.firstName,
+                lastName = emp.lastName,
+                Salary = emp.Salary,
+
+            }).ToArray();
+            Console.WriteLine();
+            foreach (var item in result2)
+            {
+                Console.WriteLine($"FirstName:{item.firstName}, LastName:{item.lastName}, Salary:{item.Salary}");
+            }
+        }
+        static void SelectMany1()
+        {// query syntax
+            List<string> strings = new List<string>() { "Shohboz", "Xurramov" };
+
+            var result = (from item in strings
+                          from item2 in item
+                          select item2).ToList();
+
+            foreach (var item in result)
             {
                 Console.WriteLine(item);
+            }
+            // method
+            Console.WriteLine("Func");
+            var result1 = strings.SelectMany(item => item.ToList());
+
+            foreach (var item in result1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void SelectMany2()
+        {
+            // query
+            var result1 = (from item1 in Emp.Employees()
+                           from item2 in item1.programming
+                           select item2).ToList();
+            foreach (var item in result1)
+            {
+                Console.WriteLine(item);
+            }
+            // method
+            Console.WriteLine("Select many");
+
+            var result2 = Emp.Employees().SelectMany(x => x.programming).ToList();
+
+            foreach (var item in result2)
+            { Console.WriteLine(item); }
+        }
+        static void SelectMany3()
+        {
+            var result1 = Emp.Employees().SelectMany(emp => emp.programming,
+                (emp, program) => new
+                {
+                    EmplyeName = emp.firstName + " " + emp.lastName,
+                    Program = program
+                }).ToList();
+            foreach (var item in result1)
+            {
+                Console.WriteLine(item.EmplyeName + "=>" + item.Program);
+            }
+            // query
+            Console.WriteLine();
+            Console.WriteLine("Query->");
+            var result2 = (from item1 in Emp.Employees()
+                           from program in item1.programming
+                           select new
+                           {
+                               EmployeName = item1.firstName + " " + item1.lastName,
+                               Programming = program
+                           }).ToList();
+            foreach (var item in result2)
+            {
+                Console.WriteLine($"{item.EmployeName}=> {item.Programming}");
             }
         }
     }
