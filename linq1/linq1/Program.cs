58522a7e568c3bbed85d2591d671b425d3f5d0d6
Shohboz Xurramov <117597489+ShokhbozKh@users.noreply.github.com>
2024-01-24 07:42:58 +0500

    asaSS

---
+++ linq1/linq1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace linq1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+            };
+
+            var linkResult = from item in numbers
+                             where item % 2 == 0
+                             select item;
+
+            foreach (var item in linkResult)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
